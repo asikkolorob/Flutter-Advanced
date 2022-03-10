5e4dc32fc094e46aabad9f7bddbd275919101207
asikkolorob <asikkolorob@gmail.com>
2022-03-10 16:55:24 +0600

    Button Theme Aded

--- flutter_advanced/lib/presentation/theme_maneger.dart
+++ flutter_advanced/lib/presentation/theme_maneger.dart
@@ -1,43 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_advanced/presentation/fonts_maneger.dart';
-import 'package:flutter_advanced/presentation/styles_maneger.dart';
 
 import '../presentation/color_maneger.dart';
 import '../presentation/values_maneger.dart';
+import '../presentation/fonts_maneger.dart';
+import '../presentation/styles_maneger.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main Color of the app
-      primaryColor: ColorManeger.primary,
-      primaryColorLight: ColorManeger.primaryOpacity70,
-      primaryColorDark: ColorManeger.darkPrimary,
-      disabledColor: ColorManeger
-          .grey1, //will be used incase of disabled button for example
-      accentColor: ColorManeger.grey,
-      // ripple Color
+    // main Color of the app
+    primaryColor: ColorManeger.primary,
+    primaryColorLight: ColorManeger.primaryOpacity70,
+    primaryColorDark: ColorManeger.darkPrimary,
+    disabledColor:
+        ColorManeger.grey1, //will be used incase of disabled button for example
+    accentColor: ColorManeger.grey,
+
+    // ripple Color
+    splashColor: ColorManeger.primaryOpacity70,
+
+    // cart View theme
+    cardTheme: CardTheme(
+      color: ColorManeger.white,
+      shadowColor: ColorManeger.grey,
+      elevation: AppSize.s4,
+    ),
+
+    // App Bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManeger.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManeger.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManeger.white,
+        fontSize: FontSizemaneger.s16,
+      ),
+    ),
+
+    // Buttom Theme
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManeger.grey1,
+      buttonColor: ColorManeger.primary,
       splashColor: ColorManeger.primaryOpacity70,
+    ),
 
-      // cart View theme
-      cardTheme: CardTheme(
-          color: ColorManeger.white,
-          shadowColor: ColorManeger.grey,
-          elevation: AppSize.s4),
-
-      // App Bar theme
-      appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManeger.primary,
-        elevation: AppSize.s4,
-        shadowColor: ColorManeger.primaryOpacity70,
-        titleTextStyle: getRegularStyle(
-          color: ColorManeger.white,
-          fontSize: FontSizemaneger.s16,
+    // Elevated Buttons Theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(color: ColorManeger.white),
+        primary: ColorManeger.primary,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(AppSize.s16),
+          ),
         ),
-      )
-      // Bottom Theme
+      ),
+    ),
+    // Text Theme
 
-      // Text Theme
-
-      // Input Decuration theme (text form fild)
-      );
+    // Input Decuration theme (text form fild)
+  );
 }
