1bef8a9b5ab0b3f9703af9be9b7f503cced4fd25
asikkolorob <asikkolorob@gmail.com>
2022-03-21 08:55:21 +0600

    Text Theme Aded

--- flutter_advanced/lib/presentation/theme_maneger.dart
+++ flutter_advanced/lib/presentation/theme_maneger.dart
@@ -7,58 +7,70 @@ import '../presentation/styles_maneger.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main Color of the app
-    primaryColor: ColorManeger.primary,
-    primaryColorLight: ColorManeger.primaryOpacity70,
-    primaryColorDark: ColorManeger.darkPrimary,
-    disabledColor:
-        ColorManeger.grey1, // will be used incase of disabled button for example
-    accentColor: ColorManeger.grey,
+      // main Color of the app
+      primaryColor: ColorManeger.primary,
+      primaryColorLight: ColorManeger.primaryOpacity70,
+      primaryColorDark: ColorManeger.darkPrimary,
+      disabledColor: ColorManeger
+          .grey1, // will be used incase of disabled button for example
+      accentColor: ColorManeger.grey,
 
-    // ripple Color
-    splashColor: ColorManeger.primaryOpacity70,
-
-    // cart View theme
-    cardTheme: CardTheme(
-      color: ColorManeger.white,
-      shadowColor: ColorManeger.grey,
-      elevation: AppSize.s4,
-    ),
-
-    // App Bar theme
-    appBarTheme: AppBarTheme(
-      centerTitle: true,
-      color: ColorManeger.primary,
-      elevation: AppSize.s4,
-      shadowColor: ColorManeger.primaryOpacity70,
-      titleTextStyle: getRegularStyle(
-        color: ColorManeger.white,
-        fontSize: FontSizemaneger.s16,
-      ),
-    ),
-
-    // Buttom Theme
-    buttonTheme: ButtonThemeData(
-      shape: const StadiumBorder(),
-      disabledColor: ColorManeger.grey1,
-      buttonColor: ColorManeger.primary,
+      // ripple Color
       splashColor: ColorManeger.primaryOpacity70,
-    ),
 
-    // Elevated Buttons Theme
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        textStyle: getRegularStyle(color: ColorManeger.white),
-        primary: ColorManeger.primary,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(AppSize.s16),
+      // cart View theme
+      cardTheme: CardTheme(
+        color: ColorManeger.white,
+        shadowColor: ColorManeger.grey,
+        elevation: AppSize.s4,
+      ),
+
+      // App Bar theme
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManeger.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManeger.primaryOpacity70,
+        titleTextStyle: getRegularStyle(
+          color: ColorManeger.white,
+          fontSize: FontSizemaneger.s16,
+        ),
+      ),
+
+      // Buttom Theme
+      buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        disabledColor: ColorManeger.grey1,
+        buttonColor: ColorManeger.primary,
+        splashColor: ColorManeger.primaryOpacity70,
+      ),
+
+      // Elevated Buttons Theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: getRegularStyle(color: ColorManeger.white),
+          primary: ColorManeger.primary,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(AppSize.s16),
+            ),
           ),
         ),
       ),
-    ),
-    // Text Theme
-
-    // Input Decuration theme (text form fild)
-  );
+      // Text Theme
+      textTheme: TextTheme(
+        headline1: getSemiBoldStyle(
+          color: ColorManeger.darkGrey,
+          fontSize: FontSizemaneger.s16,
+        ),
+        subtitle1: getMediumStyle(
+          color: ColorManeger.lightGrey,
+          fontSize: FontSizemaneger.s14,
+        ),
+        caption: getRegularStyle(color: ColorManeger.grey1),
+        bodyText1: getRegularStyle(color: ColorManeger.grey),
+        
+      )
+      // Input Decuration theme (text form fild)
+      );
 }
