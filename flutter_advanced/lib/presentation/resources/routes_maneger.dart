563180b123238de98c0c183a2f50c3203b9eb984
asikkolorob <asikkolorob@gmail.com>
2022-03-21 10:09:51 +0600

    finish adding Routes maneger

--- flutter_advanced/lib/presentation/resources/routes_maneger.dart
+++ flutter_advanced/lib/presentation/resources/routes_maneger.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../presentation/splash/splash.dart';
+import '../../presentation/forgot_password/forgot_password.dart';
+import '../../presentation/login/login.dart';
+import '../../presentation/main/main_view.dart';
+import '../../presentation/onboarding/onboarding.dart';
+import '../../presentation/register/register.dart';
+
 class Routes {
   static const String splashRoute = '/';
   static const String onBoardingRoute = '/onboarding';
@@ -6,4 +15,39 @@ class Routes {
   static const String mainRoute = '/main';
   static const String storeDetailsRoute = '/storeDetails';
   static const String forgotPasswordRoute = '/forgotPassword';
+}
+
+class RoutesGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      default:
+        return UnDefinedRoute();
+    }
+  }
+
+  // ignore: non_constant_identifier_names
+  static Route<dynamic> UnDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('No Route Found!'),
+        ),
+        body: const Center(child: Text("No Route Found!"),),
+      ),
+    );
+  }
 }
